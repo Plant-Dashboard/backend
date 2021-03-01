06bbe2c89fd6f34e6dd396f84cfad72e4731d643
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-03-01 10:36:58 -0500

    Added protect and authorize to all routes that need it

--- routes/readings.js
+++ routes/readings.js
@@ -6,12 +6,16 @@ const {
   updateReading,
   deleteReading,
 } = require("../controllers/readings");
-
 const advancedResults = require("../middleware/advancedResults");
 const Reading = require("../models/Reading");
 const router = express.Router();
+const {protect, authorize} = require("../middleware/auth");
 
-router.route("/").get(advancedResults(Reading), getReadings).post(createReading);
-router.route("/:id").get(getReading).put(updateReading).delete(deleteReading);
+router.route("/").get(advancedResults(Reading), getReadings).post(protect, authorize("admin"), createReading);
+router
+  .route("/:id")
+  .get(getReading)
+  .put(protect, authorize("admin"), updateReading)
+  .delete(protect, authorize("admin"), deleteReading);
 
 module.exports = router;
